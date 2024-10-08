6f7231071308241cbf0e3fc17866f37a5a6a413a
Spencer <spencer.e.bailey.3@gmail.com>
2024-10-08 08:28:59 -0400

    Added Neovim

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs;
   [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     fastfetch
     google-chrome
